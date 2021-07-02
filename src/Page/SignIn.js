4d7167b6ae519984d32b9b5e1fb55e7df4cbd222
Shiina <shiinahiiragi@outlook.com>
2021-07-02 14:41:20 +0800

    close the panel

--- src/Page/SignIn.js
+++ src/Page/SignIn.js
@@ -2,14 +2,13 @@ import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import Title from "../Component/Title";
-import SignInForm from "../Unit/Form";
-import Copyright from "../Component/Copyright";
+import SignInPaper from "../Unit/SignInPaper";
 
 import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: "100vh"
+    height: "100vh",
+    userSelect: "none"
   },
   image: {
     backgroundImage: `url(${requestURL}/src/cover)`,
@@ -20,12 +19,6 @@ const useStyles = makeStyles((theme) => ({
         : theme.palette.grey[900],
     backgroundSize: "cover",
     backgroundPosition: "center"
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
   }
 }));
 
@@ -34,16 +27,11 @@ const requestURL = isDevMode ? "http://localhost:8000" : "";
 
 export default function SignIn() {
   const classes = useStyles();
-
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Title />
-          <SignInForm />
-          <Copyright />
-        </div>
+        <SignInPaper />
       </Grid>
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
     </Grid>
