c1fd7cc8369eef9d7a1c15580b8d7d18a9736ad9
Shiina <shiinahiiragi@outlook.com>
2021-07-03 19:37:48 +0800

    stage db insert

--- server/bin/db.js
+++ server/bin/db.js
@@ -1,31 +1,60 @@
 const Pool = require('pg').Pool
 const fs = require('fs');
 const path = require('path');
-const syncEachChain = require('./api').syncEachChain;
 
 const setting = JSON.parse(fs.readFileSync(path.join(__dirname, './setting.json')))
 const pool = new Pool(setting.poolSetting);
-const outerQuery = (sql) => new Promise((resolve, reject) => {
+const wrapQuery = (sql) => new Promise((resolve, reject) => {
   pool.query(sql, (err, res) => {
     if (err) reject(err);
     else resolve(res.rows);
   });
 });
+const syncEachChain = (arrayObject, eachTemp) =>
+arrayObject.reduce(
+  (promiseChain, arrayItem) =>
+    promiseChain.then(() => new Promise((resolve, reject) =>
+      eachTemp(arrayItem, resolve, reject))),
+  Promise.resolve()
+);
 
-exports.query = outerQuery;
+exports.query = wrapQuery;
+exports.syncEachChain = syncEachChain;
 exports.dbInitialize = (clearAll) => new Promise((resolve, reject) => {
   const newTable = () => {
     const schemaSQL = Object.values(setting.schema).map(item => item.join(' '));
     syncEachChain(schemaSQL, (item, onsuccess, onerror) => {
-      outerQuery(item).then(onsuccess).catch(onerror);
+      wrapQuery(item).then(onsuccess).catch(onerror);
     });
   }
   if (clearAll) {
     const schemas = Object.keys(setting.schema).reverse();
     syncEachChain(schemas, (item, onsuccess, onerror) => {
-      outerQuery(`drop table if exists ${item}`)
-        .then(onsuccess)
-        .catch(onerror);
+      wrapQuery(`drop table if exists ${item}`)
+        .then(onsuccess).catch(onerror);
     }).then(newTable);
   } else newTable();
 });
+
+exports.exec = (cmdLine) => new Promise((resolve, reject) => {
+  if (cmdLine[0] === '') cmdLine.shift();
+  if (cmdLine[cmdLine.length - 1] === '') cmdLine.pop();
+  if (cmdLine[0] === 'sign') {
+    if (cmdLine.length < 4) {
+      reject('ERROR: too few arguments.\n  Try using sign <email> <username> <password>');
+    } else {
+      let returnUserID;
+      wrapQuery(`insert into userInfo(email)
+        values('${cmdLine[1]}')
+        returning userID`)
+        .then((res) => {
+          returnUserID = res[0].userid;
+          return wrapQuery(`insert into userSetting(userID, username, password, gender)
+            values(${returnUserID}, '${cmdLine[2]}', '${cmdLine[3]}', 'U')`)
+        }).then(() => {
+          console.log(`INSERT userID: ${returnUserID}`);
+          resolve();
+        }).catch(err => reject(err));
+    }
+  } else reject('ERROR: cannot parse the command.');
+})
