96db8b3d37a0efea164a1434f4a21cd4e6e8fc8f
Shiina <shiinahiiragi@outlook.com>
2021-07-03 20:06:32 +0800

    add sign in

---
+++ server/bin/api.js
@@ -0,0 +1,28 @@
+const SHA256 = require('crypto-js').SHA256;
+
+exports.syncEachChain = (arrayObject, eachTemp) =>
+arrayObject.reduce(
+  (promiseChain, arrayItem) =>
+    promiseChain.then(() => new Promise((resolve, reject) =>
+      eachTemp(arrayItem, resolve, reject))),
+  Promise.resolve()
+);
+
+exports.checkRegister = (cmdLine) => new Promise((resolve, reject) => {
+  if (cmdLine.length < 4)
+    reject('ERROR: too few arguments.\n  Try using sign <email> <username> <password>');
+  else if (cmdLine[1].length > 32)
+    reject('ERROR: The length of E-mail is out of range.');
+  else if (!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(cmdLine[1]))
+    reject('ERROR: Unsupported E-mail address.');
+  else if (cmdLine[2].length > 16)
+    reject('ERROR: The length of username is out of range.');
+  else if (!/^[\x0-\x7F]*$/.test(cmdLine[2]))
+    reject('ERROR: The username contains illegal characters.');
+  else if (cmdLine[3].length > 32 || cmdLine[3].length < 8)
+    reject('ERROR: The length of password is out of range.');
+  else {
+    cmdLine[3] = SHA256(cmdLine[1] + cmdLine[3]).toString();
+    resolve(cmdLine);
+  }
+})
--- server/bin/db.js
+++ server/bin/db.js
@@ -1,6 +1,7 @@
 const Pool = require('pg').Pool
 const fs = require('fs');
 const path = require('path');
+const api = require('./api');
 
 const setting = JSON.parse(fs.readFileSync(path.join(__dirname, './setting.json')))
 const pool = new Pool(setting.poolSetting);
@@ -10,26 +11,33 @@ const wrapQuery = (sql) => new Promise((resolve, reject) => {
     else resolve(res.rows);
   });
 });
-const syncEachChain = (arrayObject, eachTemp) =>
-arrayObject.reduce(
-  (promiseChain, arrayItem) =>
-    promiseChain.then(() => new Promise((resolve, reject) =>
-      eachTemp(arrayItem, resolve, reject))),
-  Promise.resolve()
-);
+
+const insertUser = (cmdLine, onsuccess, onerror) => {
+  let returnUserID;
+  wrapQuery(`insert into userInfo(email)
+    values('${cmdLine[1]}')
+    returning userID`)
+    .then((res) => {
+      returnUserID = res[0].userid;
+      return wrapQuery(`insert into userSetting(userID, username, password, gender)
+        values(${returnUserID}, '${cmdLine[2]}', '${cmdLine[3]}', 'U')`)
+    }).then(() => {
+      console.log(`INSERT userID: ${returnUserID}`);
+      onsuccess();
+    }).catch(err => onerror(err));
+}
 
 exports.query = wrapQuery;
-exports.syncEachChain = syncEachChain;
 exports.dbInitialize = (clearAll) => new Promise((resolve, reject) => {
   const newTable = () => {
     const schemaSQL = Object.values(setting.schema).map(item => item.join(' '));
-    syncEachChain(schemaSQL, (item, onsuccess, onerror) => {
+    api.syncEachChain(schemaSQL, (item, onsuccess, onerror) => {
       wrapQuery(item).then(onsuccess).catch(onerror);
     });
   }
   if (clearAll) {
     const schemas = Object.keys(setting.schema).reverse();
-    syncEachChain(schemas, (item, onsuccess, onerror) => {
+    api.syncEachChain(schemas, (item, onsuccess, onerror) => {
       wrapQuery(`drop table if exists ${item}`)
         .then(onsuccess).catch(onerror);
     }).then(newTable);
@@ -40,21 +48,8 @@ exports.exec = (cmdLine) => new Promise((resolve, reject) => {
   if (cmdLine[0] === '') cmdLine.shift();
   if (cmdLine[cmdLine.length - 1] === '') cmdLine.pop();
   if (cmdLine[0] === 'sign') {
-    if (cmdLine.length < 4) {
-      reject('ERROR: too few arguments.\n  Try using sign <email> <username> <password>');
-    } else {
-      let returnUserID;
-      wrapQuery(`insert into userInfo(email)
-        values('${cmdLine[1]}')
-        returning userID`)
-        .then((res) => {
-          returnUserID = res[0].userid;
-          return wrapQuery(`insert into userSetting(userID, username, password, gender)
-            values(${returnUserID}, '${cmdLine[2]}', '${cmdLine[3]}', 'U')`)
-        }).then(() => {
-          console.log(`INSERT userID: ${returnUserID}`);
-          resolve();
-        }).catch(err => reject(err));
-    }
+    api.checkRegister(cmdLine)
+      .then((newLine) => insertUser(newLine, resolve, reject))
+      .catch(err => reject(err));
   } else reject('ERROR: cannot parse the command.');
 })
