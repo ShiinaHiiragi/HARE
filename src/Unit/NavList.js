691be131d262307558ed41eed516a25a9b5a69b5
Shiina <shiinahiiragi@outlook.com>
2021-07-06 21:36:59 +0800

    subtle change

--- src/Unit/NavList.js
+++ src/Unit/NavList.js
@@ -42,7 +42,6 @@ export default function NavList(props) {
           toggleMessageBox: handle.toggleMessageBox,
           toggleKick: handle.toggleKick
         }}
-        toggleMessageBox={handle.toggleMessageBox}
       />
     </div>
   );
