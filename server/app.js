7b088d4e41e8b461667fae02e51120f383e6ec36
Shiina <shiinahiiragi@outlook.com>
2021-08-05 15:01:44 +0800

    add 404

--- server/app.js
+++ server/app.js
@@ -13,15 +13,12 @@ var setRouter = require('./routes/set');
 var newRouter = require('./routes/new');
 var deleteRouter = require('./routes/delete');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'view')));
 if (!process.argv.find(item => item === "--disable-cors"))
   app.use(cors({
     origin: ['http://localhost:3000'],
@@ -50,7 +47,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.sendFile(path.join(__dirname, `./build/index.html`));
+  res.sendFile(path.join(__dirname, `./view/404.html`));
 });
 
 module.exports = app;
