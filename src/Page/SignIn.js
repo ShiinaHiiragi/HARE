3b421f6b35302ba8d855844308fd58b8c9574b48
Shiina <shiinahiiragi@outlook.com>
2022-12-01 18:08:28 +0800

    Fix: interface of signin page

--- src/Page/SignIn.js
+++ src/Page/SignIn.js
@@ -10,7 +10,16 @@ import MessageBox from "../Dialogue/MessageBox";
 import Load from "../Dialogue/Load";
 import requestURL from "../Interface/Constant";
 import { languagePicker, nameMap } from "../Language/Lang";
-import { cookieTime, setStateDelay } from "../Interface/Constant";
+import { ThemeProvider } from "@material-ui/core/styles";
+import {
+  palette,
+  cookieTime,
+  cookieString,
+  defaultPrimaryColor,
+  defaultSecondaryColor,
+  setStateDelay,
+  getColorTheme
+} from "../Interface/Constant";
 
 import makeStyles from "@material-ui/core/styles/makeStyles";
 const useStyles = makeStyles((theme) => ({
@@ -46,15 +55,34 @@ export default function SignIn() {
 
   // the state of language
   const [globalLang, setGlobalLang] = React.useState(languagePicker(nameMap.English));
+  const [primaryColor, setPrimaryColor] = React.useState(defaultPrimaryColor);
+  const [secondaryColor, setSecondaryColor] = React.useState(defaultSecondaryColor);
   React.useEffect(() => {
     let storageLang = cookie.load("lang");
     changeGlobalLang(storageLang || nameMap.English);
+    cookieString(
+      "primary",
+      setPrimaryColor,
+      defaultPrimaryColor,
+      (cookie) => Object.keys(palette).includes(cookie)
+    );
+    cookieString(
+      "secondary",
+      setSecondaryColor,
+      defaultSecondaryColor,
+      (cookie) => Object.keys(palette).includes(cookie)
+    );
   }, []);
   const changeGlobalLang = (targetValue) => {
     if (targetValue)
       setTimeout(() => setGlobalLang(languagePicker(targetValue)), setStateDelay * 0.5);
     cookie.save("lang", targetValue, { expires: cookieTime(3650) });
   };
+
+  const colorTheme = React.useMemo(
+    () => getColorTheme(primaryColor, secondaryColor),
+    [primaryColor, secondaryColor]
+  );
 
   // the setting of snackbar
   const [messageBoxInfo, setMessageBoxInfo] = React.useState({
@@ -90,37 +118,39 @@ export default function SignIn() {
   };
 
   return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <SignTitle lang={globalLang} />
-          <SignInForm
-            lang={globalLang}
-            handle={{
-              toggleMessageBox: toggleMessageBox,
-              changeLang: changeGlobalLang,
-              toggleLoading: toggleLoading,
-              closeLoading: closeLoading
-            }}
-            URL={requestURL}
-          />
-          <Copyright
-            lang={globalLang}
-            handleToggleMessageBox={toggleMessageBox}
-          />
-        </div>
+    <ThemeProvider theme={colorTheme}>
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <SignTitle lang={globalLang} />
+            <SignInForm
+              lang={globalLang}
+              handle={{
+                toggleMessageBox: toggleMessageBox,
+                changeLang: changeGlobalLang,
+                toggleLoading: toggleLoading,
+                closeLoading: closeLoading
+              }}
+              URL={requestURL}
+            />
+            <Copyright
+              lang={globalLang}
+              handleToggleMessageBox={toggleMessageBox}
+            />
+          </div>
+        </Grid>
+        <Grid item xs={false} sm={4} md={7} className={classes.image}>
+          <div className={classes.overlay} />
+        </Grid>
+        <MessageBox
+          open={messageBoxInfo.open}
+          handleClose={closeMessageBox}
+          messageBoxType={messageBoxInfo.type}
+          messageBoxMessage={messageBoxInfo.message}
+        />
+        <Load open={loading} />
       </Grid>
-      <Grid item xs={false} sm={4} md={7} className={classes.image}>
-        <div className={classes.overlay} />
-      </Grid>
-      <MessageBox
-        open={messageBoxInfo.open}
-        handleClose={closeMessageBox}
-        messageBoxType={messageBoxInfo.type}
-        messageBoxMessage={messageBoxInfo.message}
-      />
-      <Load open={loading} />
-    </Grid>
+    </ThemeProvider>
   );
 }
